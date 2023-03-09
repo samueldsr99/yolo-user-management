bb4ea9fd2cc90cb8c2dff722a47602177d0f4aac
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-03-09 18:28:33 -0500

    bugfix(frontend): Add action to prefetchQuery

--- client/src/pages/loader.ts
+++ client/src/pages/loader.ts
@@ -1,7 +1,8 @@
 import queryClient from "../config/react-query.config";
+import { listGames } from "../lib/api/games";
 import * as querykeys from "../lib/api/querykeys";
 
 export default async function loader() {
-  await queryClient.prefetchQuery(querykeys.listGames());
+  await queryClient.prefetchQuery(querykeys.listGames(), () => listGames());
   return null;
 }

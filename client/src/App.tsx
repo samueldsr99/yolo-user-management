1aebf0456aedfe1dc4296e5452f67a4764332cde
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-03-09 12:34:11 -0500

    feat(frontend): Add react query config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,23 @@
 import { Suspense } from "react";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import queryClient from "./config/react-query.config";
 import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <Suspense fallback={<>Loading...</>}>
-      <RouterProvider router={router} />
-    </Suspense>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<>Loading...</>}>
+        <RouterProvider router={router} />
+      </Suspense>
+      {import.meta.env.DEV ? (
+        <ReactQueryDevtools initialIsOpen={false} />
+      ) : null}
+    </QueryClientProvider>
   );
 }
 

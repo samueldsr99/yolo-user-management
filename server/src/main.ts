44fb681efb136ef404c117c1dc6401a5040005cc
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-03-09 16:44:45 -0500

    feat(backend): Set global prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('/v1/api');
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();

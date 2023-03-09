46cb51d1d877ee1ed45d72cd709fb8d596aeabd2
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-03-09 18:23:26 -0500

    style(frontend): Add styles for `Button`

--- client/src/components/common/button/Button.tsx
+++ client/src/components/common/button/Button.tsx
@@ -1,9 +1,63 @@
-type ButtonProps = JSX.IntrinsicElements["button"] & {
+import * as React from "react";
+import clsx from "classnames/bind";
+
+import Spinner from "../spinner";
+
+export type ButtonProps = JSX.IntrinsicElements["button"] & {
+  variant?: "primary" | "secondary";
+  size?: "sm" | "md" | "lg" | "xl";
+  submit?: boolean;
   isLoading?: boolean;
+  textCentered?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const classes = clsx.bind({
+  root: "rounded-md font-semibold border-none",
+  animated: "transition-shadow hover:shadow-xl active:opacity-90",
+  primary: "text-slate-400 bg-gray-800 border border-gray-600",
+  secondary: "text-slate-900 bg-gray-300",
+  disabled: "opacity-70",
+  sm: "py-1 px-2",
+  md: "py-2 px-4",
+  lg: "py-3 px-6",
+  xl: "py-4 px-8",
+});
+
+const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  size = "md",
+  disabled,
+  className,
+  submit = false,
+  textCentered = false,
+  isLoading,
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      type={submit ? "submit" : "button"}
+      className={classes(
+        "root",
+        variant,
+        !disabled && "animated",
+        size,
+        disabled && "disabled",
+        className
+      )}
+      {...props}
+    >
+      <div
+        className={classes(
+          "w-auto flex items-center gap-2",
+          textCentered && "justify-center"
+        )}
+      >
+        {isLoading && <Spinner size="xs" />}
+        {children}
+      </div>
+    </button>
+  );
 };
 
 export default Button;

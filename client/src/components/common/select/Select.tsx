9ac3f100de3f8fbb0bc44641d6dd191f073b2330
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-03-13 22:30:16 -0400

    styles(frontend): Update styles for `Select` component

--- client/src/components/common/select/Select.tsx
+++ client/src/components/common/select/Select.tsx
@@ -3,7 +3,7 @@ import clsx from "classnames/bind";
 export type SelectProps = JSX.IntrinsicElements["select"] & {};
 
 const classes = clsx.bind({
-  root: "mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6",
+  root: "block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6",
 });
 
 const Select: React.FC<SelectProps> = ({ className, children, ...props }) => {
